ebcbe5942108286101a8b2e3267f4767479c5eab
Chase McCoy <chasem000@gmail.com>
2017-11-18 12:22:02 -0600

    Responsiveness improvements

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import styled from 'styled-components'
+import styled, {ThemeProvider} from 'styled-components'
+import { sizes } from '../utils/design'
 
 import {Link} from '../components/Components'
 import {Wrapper, Content} from '../components/Layout'
 import Sidebar from '../components/Sidebar'
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Chase McCoy"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
+  <ThemeProvider
+    theme={{
+      breakpoints: [
+        sizes.breakpoints.small,
+        sizes.breakpoints.medium,
+        sizes.breakpoints.large
+      ]
+    }}
+  >
+    <div>
+      <Helmet
+        title="Chase McCoy"
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
 
-    <Wrapper>
-      <Sidebar />
+      <Wrapper>
+        <Sidebar />
 
-      <Content>
-        {children()}
-      </Content>
-    </Wrapper>
-  </div>
+        <Content>
+          {children()}
+        </Content>
+      </Wrapper>
+    </div>
+  </ThemeProvider>
 )
 
 TemplateWrapper.propTypes = {
