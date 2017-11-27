feae4cd08c0ebe29ab02f9c9868678716cd829b3
Chase McCoy <chasem000@gmail.com>
2017-11-27 13:13:21 -0600

    layout tweaks

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,6 +19,6 @@ export const Content = styled.div.attrs({
 
 	${media.small`
 		width: 100%;
-		padding: 120px ${sizes.content.smallPadding} 0;
+		padding: 88px ${sizes.content.smallPadding} 0;
 	`}
 `
