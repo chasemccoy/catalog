cca70b736057445a99c712339ab07d5c21ebe480
Chase McCoy <chasem000@gmail.com>
2017-12-03 16:11:40 -0600

    Markdown component

---
+++ src/components/Markdown.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReactMarkdown = require('react-markdown')
+
+class Markdown extends React.Component {
+	render() {
+		return <ReactMarkdown {...this.props} source={this.props.children} />
+	}
+}
+
+export default Markdown
