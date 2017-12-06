4401c03a8d75eed31edf931570e674316596f0da
Chase McCoy <chasem000@gmail.com>
2017-12-06 15:43:17 -0600

    ~

--- src/components/Image.js
+++ src/components/Image.js
@@ -7,12 +7,14 @@ const StyledImage = styled(Img)`
 	border-radius: 4px;
 	max-width: 100%;
 	position: initial !important;
+	margin-bottom: 0;
 
 	${'' /* background: linear-gradient(to bottom, rgba(255, 255, 255, 0.55) 0%, rgba(255, 255, 255, 0) 3%);
 	box-shadow: 0 2px 6px 0 rgba(0,0,0,0.35); */}
 
 	img {
 		border-radius: 4px;
+		margin-bottom: 0;
 	}
 `
 
