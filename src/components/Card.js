2287b766a2d1f1c1e8187d0f263369872aae210f
Chase McCoy <chasem000@gmail.com>
2017-12-07 11:56:12 -0600

    fucking dope

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,33 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from './Components'
 import { colors, sizes } from '../utils/design'
+import { media } from '../utils/media'
+
+const largeMarginFix = () => {
+  return `
+		margin-left: ${'-' + sizes.library.padding.large};
+		margin-right: ${'-' + sizes.library.padding.large};
+  `;
+}
+
+const smallMarginFix = () => {
+  return `
+		width: calc(100% + ${sizes.library.padding.small} * 2);
+		margin-left: ${'-' + sizes.library.padding.small};
+		margin-right: ${'-' + sizes.library.padding.small};
+  `;
+}
 
 const DivCard = styled.div`
 	display: block;
 	border-radius: 4px;
-
 	background-color: ${props => props.highlight ? colors.bookmark.background : 'white'};
 	border: ${props => props.highlight ? ('1px solid ' + colors.bookmark.border.outer) : '1px solid transparent'};
+
+	${props => !props.highlight && largeMarginFix()};
+	${props => !props.highlight && media.tiny`
+		${smallMarginFix()}
+	`}
 
 	&:hover {
 		background-color: ${colors.bookmark.background};
