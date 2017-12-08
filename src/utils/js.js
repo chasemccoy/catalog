d224fcc03322b6e8be724bbfa9357ec8fa4ee1a0
Chase McCoy <chasem000@gmail.com>
2017-12-08 13:40:42 -0600

    table of contents

--- src/utils/js.js
+++ src/utils/js.js
@@ -1,3 +1,7 @@
 export function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function lowercase(string) {
+  return string.charAt(0).toLowerCase() + string.slice(1);
+}
