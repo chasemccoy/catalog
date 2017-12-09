ff6a0db50354b88ed7401445bf4f6695452a7c3d
Chase McCoy <chasem000@gmail.com>
2017-12-08 22:13:36 -0600

    stuff

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,16 @@ import { media } from '../utils/media'
 
 const largeMarginFix = () => {
   return `
-		margin-left: ${'-' + sizes.library.padding.large};
-		margin-right: ${'-' + sizes.library.padding.large};
+		margin-left: ${'-' + sizes.card.padding.large};
+		margin-right: ${'-' + sizes.card.padding.large};
   `;
 }
 
 const smallMarginFix = () => {
   return `
-		width: calc(100% + ${sizes.library.padding.small} * 2);
-		margin-left: ${'-' + sizes.library.padding.small};
-		margin-right: ${'-' + sizes.library.padding.small};
+		width: calc(100% + ${sizes.card.padding.small} * 2);
+		margin-left: ${'-' + sizes.card.padding.small};
+		margin-right: ${'-' + sizes.card.padding.small};
   `;
 }
 
@@ -23,11 +23,14 @@ const DivCard = styled.div`
 	display: block;
   min-height: 100%;
 	border-radius: 4px;
+  padding: ${sizes.card.padding.large};
 	background-color: ${props => props.highlight ? colors.bookmark.background : 'white'};
 	border: ${props => props.highlight ? ('1px solid ' + colors.bookmark.border.outer) : '1px solid transparent'};
+  color: ${colors.text.header};
 
 	${props => !props.highlight && largeMarginFix()};
 	${props => !props.highlight && media.tiny`
+    padding: ${sizes.card.padding.small};
 		${smallMarginFix()}
 	`}
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 import Page from '../components/Page'
 import { Link } from '../components/Components'
-import Bookmark from '../components/Bookmark'
+import List from '../components/Lists'
+import Card from '../components/Card'
 import Image from '../components/Image'
 import { Row, Column } from '../components/Grid'
+import { colors, fontWeights } from '../utils/design'
+
+const Token = styled(Card).attrs({
+	highlight: 'true',
+})`
+	padding: 8px;
+  font-size: 14px;
+  color: ${colors.sidebar.link.selected};
+  font-weight: ${fontWeights.medium};
+  letter-spacing: .03em;
+  line-height: 1.3;
+`
 
 const IndexPage = ({data}) => {
   return (
@@ -11,12 +25,50 @@ const IndexPage = ({data}) => {
       title='Chase McCoy is a design developer living in Chicago that spends a lot of time thinking about how the web works.'
     >
       <Row mb={40}>
-        <Column width={[2/5]}>
+        <Column width={[1/2, 1/3]}>
+          <Token>Chicago, IL</Token>
+        </Column>
+
+        <Column width={[1/2, 1/3]}>
+          <Token>@chase_mccoy</Token>
+        </Column>
+
+        <Column width={[1, 1/3]}>
+          <Token>desk@chasemccoy.net</Token>
+        </Column>
+      </Row>
+
+      <Row mb={40}>
+        <Column width={[1.8/5]}>
           <Image src={'/meta/chase.jpg'} />
         </Column>
 
-        <Column width={[3/5]}>
+        <Column width={[3.2/5]}>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Column>
+      </Row>
+
+      <List ordered highlight>
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+      </List>
+
+      <Row mb={40}>
+        <Column width={[1, 1/2]}>
+          <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
+        </Column>
+
+        <Column width={[1, 1/2]}>
+          <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
+        </Column>
+
+        <Column width={[1, 1/2]}>
+          <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
+        </Column>
+
+        <Column width={[1, 1/2]}>
+          <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
         </Column>
       </Row>
 
