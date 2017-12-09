a9a452d5f8b0f5f11a27a897694aeb8b41c8443a
Chase McCoy <chasem000@gmail.com>
2017-12-09 13:21:24 -0600

    index header

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,16 @@ import List from '../components/Lists'
 import Card from '../components/Card'
 import Image from '../components/Image'
 import { Row, Column } from '../components/Grid'
-import { colors, fontWeights } from '../utils/design'
+import { colors, sizes, fontWeights } from '../utils/design'
+
+const Portrait = styled(Image)`
+  width: 100%;
+  border-radius: 50%;
+`
+
+const PageTitle = styled.h2`
+  margin: 0;
+`
 
 const Token = styled(Card).attrs({
 	highlight: 'true',
@@ -19,72 +28,128 @@ const Token = styled(Card).attrs({
   line-height: 1.3;
 `
 
-const IndexPage = ({data}) => {
-  return (
-    <Page
-      title='Chase McCoy is a design developer living in Chicago that spends a lot of time thinking about how the web works.'
-    >
-      <Row mb={40}>
-        <Column width={[1/2, 1/3]}>
-          <Token>Chicago, IL</Token>
+const Header = (props) => {
+  if (props.isSmall) {
+    return (
+      <div>
+        <Row mb={16}>
+          <Column width={[1/5]}>
+            {props.image}
+          </Column>
+
+          <Column width={[4/5]}>
+            <PageTitle>{props.title}</PageTitle>
+          </Column>
+        </Row>
+
+        <Row mb={40}>
+          {props.tokens.map((token) =>
+            <Column width={[1/2, 1/3]}>
+              {token}
+            </Column>
+          )}
+        </Row>
+      </div>
+    )
+  }
+  else {
+    return (
+      <Row mb={24}>
+        <Column width={[1/5]}>
+          {props.image}
         </Column>
 
-        <Column width={[1/2, 1/3]}>
-          <Token>@chase_mccoy</Token>
-        </Column>
+        <Column width={[4/5]}>
+          <Row mb={40}>
+            <Column width={1}>
+              <PageTitle>{props.title}</PageTitle>
+            </Column>
 
-        <Column width={[1, 1/3]}>
-          <Token>desk@chasemccoy.net</Token>
-        </Column>
-      </Row>
-
-      <Row mb={40}>
-        <Column width={[1.8/5]}>
-          <Image src={'/meta/chase.jpg'} />
-        </Column>
-
-        <Column width={[3.2/5]}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </Column>
-      </Row>
-
-      <List ordered highlight>
-        <li>One</li>
-        <li>Two</li>
-        <li>Three</li>
-      </List>
-
-      <Row mb={40}>
-        <Column width={[1, 1/2]}>
-          <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
-        </Column>
-
-        <Column width={[1, 1/2]}>
-          <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
-        </Column>
-
-        <Column width={[1, 1/2]}>
-          <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
-        </Column>
-
-        <Column width={[1, 1/2]}>
-          <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
+            {props.tokens.map((token) =>
+              <Column width={[1/2, 1/3]}>
+                {token}
+              </Column>
+            )}
+          </Row>
         </Column>
       </Row>
+    )
+  }
+}
 
-      {data.allMarkdownRemark.edges.map(({node}) =>
-        <div key={node.id}>
-          <article>
-            <h3>
-              <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
-            </h3>
+class IndexPage extends React.Component {
+  state = {
+    isSmall: false
+  }
 
-            <div dangerouslySetInnerHTML={{ __html: node.html }} />
-          </article>
-        </div>
-      )}
-    </Page>
-  )
+  componentDidMount = () => {
+    window.matchMedia(`(min-width: ${sizes.breakpoints.medium})`).addListener(this.mediaQueryChanged)
+
+    this.mediaQueryChanged()
+  }
+
+  mediaQueryChanged = () => {
+    this.setState({isSmall: !window.matchMedia(`(min-width: ${sizes.breakpoints.medium})`).matches})
+  }
+
+  render() {
+    return (
+      <Page>
+        <Header
+          isSmall={this.state.isSmall}
+          title='Chase McCoy is a design developer living in Chicago that spends a lot of time thinking about how the web works.'
+          image={<Portrait src='/meta/chase.jpg' />}
+          tokens={[
+            <Token>Chicago, IL</Token>,
+            <Token>@chase_mccoy</Token>,
+            <Token>desk@chasemccoy.net</Token>
+          ]}
+        />
+
+        <Row>
+          <Column>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </Column>
+        </Row>
+
+        <List ordered highlight>
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </List>
+
+        <Row mb={40}>
+          <Column width={[1, 1/2]}>
+            <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
+          </Column>
+
+          <Column width={[1, 1/2]}>
+            <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
+          </Column>
+
+          <Column width={[1, 1/2]}>
+            <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
+          </Column>
+
+          <Column width={[1, 1/2]}>
+            <Card highlight>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Card>
+          </Column>
+        </Row>
+
+        {this.props.data.allMarkdownRemark.edges.map(({node}) =>
+          <div key={node.id}>
+            <article>
+              <h3>
+                <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+              </h3>
+
+              <div dangerouslySetInnerHTML={{ __html: node.html }} />
+            </article>
+          </div>
+        )}
+      </Page>
+    )
+  }
 }
 
 export default IndexPage
