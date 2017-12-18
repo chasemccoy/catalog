622599595c394983dc8f5283b2f55421b03594ec
Chase McCoy <chasem000@gmail.com>
2017-12-17 20:41:05 -0600

    ScrollRow Components

---
+++ src/components/ScrollRow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Row, Column } from '../components/Grid'
+
+const ScrollRow = styled(Row)`
+  position: relative;
+  flex-wrap: nowrap;
+  overflow: auto;
+  -webkit-overflow-scrolling: touch;
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+  border-radius: 8px;
+  mask-image: linear-gradient(to left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 20%);
+`
+
+export default ScrollRow
