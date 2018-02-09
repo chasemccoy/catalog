8d597e5e245aba21eaa53a95ad6cb18dbddfceac
Chase McCoy <chasem000@gmail.com>
2018-02-09 12:30:43 -0600

    bleep bloop

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,4 +1,5 @@
 import Typography from 'typography'
+import { colors } from 'utils/design'
 
 const typography = new Typography(
 	{
@@ -7,11 +8,14 @@ const typography = new Typography(
 		bodyFontFamily: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif'],
 		headerFontFamily: ['Karla', 'Georgia', 'serif'],
 		scaleRatio: 2.3,
-		headerLineHeight: 1.5,
+		headerLineHeight: 1.4,
 		headerWeight: 600,
 		overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
 			a: {
 				WebkitTextDecorationSkip: 'unset'
+			},
+			'h1, h2, h3, h4, h5, h6': {
+				color: colors.text.header
 			},
 			'.sans': {
 				fontFamily: options.bodyFontFamily.join(`,`)
