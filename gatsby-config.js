80b82568166327dce3a348742725607fec85d35f
Chase McCoy <chasem000@gmail.com>
2018-02-15 23:20:16 -0600

    rss??

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Chase McCoy`,
+    description: `The thought stream of Chase McCoy.`,
+    siteUrl: `http://chasem.co/`
   },
   pathPrefix: '/',
   plugins: [
@@ -24,6 +26,57 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography.js`
       }
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+         {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }`,
+        feeds: [
+          {
+            serialize: ({query: {site, allWordpressPost}}) => {
+              return allWordpressPost.edges.map(edge => {
+                return Object.assign(
+                  {},
+                  {
+                    title: edge.node.title,
+                    description: edge.node.content,
+                    url: site.siteMetadata.siteUrl + edge.node.slug,
+                    guid: site.siteMetadata.siteUrl + edge.node.slug,
+                    date: edge.node.date,
+                    author: 'Chase McCoy'
+                  },
+                );
+              });
+            },
+            query: `
+              {
+                allWordpressPost {
+                  edges {
+                    node {
+                      title
+                      slug
+                      content
+                      excerpt
+                      date(formatString: "MMMM DD, YYYY, h:mm A")
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/feed.xml',
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-favicon`,
