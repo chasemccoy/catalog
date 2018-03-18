a5d7d9cb53ba1a7c7c8a72f8b546d8dfda9c09f5
Chase McCoy <chasem000@gmail.com>
2018-03-18 13:21:30 -0500

    how do

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -7,6 +7,7 @@ const StyledIcon = styled.object`
 	height: ${props => props.large ? `1.1em` : `1.4em`};
 	margin: ${props => props.large ? `-5px 8px 0 0` : props.small ? `-4px 6px 0 0` : `0 4px 0 0`} !important;
 	vertical-align: middle;
+	max-width: 2em;
 `
 
 const Icon = props => (
