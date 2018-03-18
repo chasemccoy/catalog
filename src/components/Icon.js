ca039e51fcc182fbe1b42270d136242c4190825a
Chase McCoy <chasem000@gmail.com>
2018-03-18 13:13:44 -0500

    sigh

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -4,8 +4,6 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledIcon = styled.object`
-	display: inline-flex;
-	flex-shrink: 0;
 	height: ${props => props.large ? `1.1em` : `1.4em`};
 	margin: ${props => props.large ? `-5px 8px 0 0` : props.small ? `-4px 6px 0 0` : `0 4px 0 0`} !important;
 	vertical-align: middle;
