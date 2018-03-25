83e953a74c59fc4735e0e42af1cd7d32550902c7
Chase McCoy <chasem000@gmail.com>
2018-03-25 17:07:20 -0500

    better quotes

--- src/components/Token.js
+++ src/components/Token.js
@@ -1,19 +1,22 @@
 import Card from 'components/Card'
 import React from 'react'
-import { fontWeights } from 'utils/design'
+import { colors, fontWeights } from 'utils/design'
 import styled from 'styled-components'
+import { space } from 'styled-system'
 
 const Token = styled(Card)`
   display: inline-flex !important;
   min-height: auto;
-	padding: 6px !important;
-  font-size: 14px;
+  font-size: 12px;
   font-weight: ${fontWeights.medium};
   letter-spacing: .03em;
   line-height: 1.3;
+  ${space}
+  border-radius: 500px;
+  color: ${colors.sidebar.link.primary};
 
-  & + & {
-    margin-left: 16px;
+  &:last-child {
+    margin-right: 0;
   }
 `
 
