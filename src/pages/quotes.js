cb5d7926eb60cc41558c7bfe958dedc2c83d14ad
Chase McCoy <chasem000@gmail.com>
2018-03-25 19:26:10 -0500

    yada yada yada

--- src/pages/quotes.js
+++ src/pages/quotes.js
@@ -9,15 +9,15 @@ import { space, borderRadius, themeGet } from 'styled-system'
 const Container = styled.div`
   ${space}
   ${borderRadius}
-  background-color: ${colors.primary.gray.light};
-  overflow: hidden;
-  border: 1px solid ${colors.primary.gray.medium};
+  ${'' /* background-color: ${colors.primary.gray.light}; */}
+  ${'' /* overflow: hidden; */}
+  ${'' /* border: 1px solid ${colors.primary.gray.medium}; */}
+  display: flex;
+  flex-direction: column;
 `
 
 const Quote = styled.h2`
   ${space}
-  color: ${colors.text.body};
-  display: flex;
 `
 
 const Meta = styled.div`
@@ -25,8 +25,8 @@ const Meta = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: ${colors.primary.gray.medium};
-  border-top: 1px solid ${colors.primary.gray.dark};
+  ${'' /* background-color: ${colors.primary.gray.medium}; */}
+  ${'' /* border-top: 1px solid ${colors.primary.gray.dark}; */}
 `
 
 const Tags = styled.div`
@@ -36,8 +36,7 @@ const Tags = styled.div`
 const Source = styled.h3`
   font-weight: ${themeGet('fontWeights.normal')};
   font-family: ${themeGet('fontFamily.sans')};
-  text-align: right;
-  color: ${colors.sidebar.link.primary};
+  color: ${colors.card.text};
   margin: 0;
   flex-basis: 60%;
   font-size: 20px;
@@ -53,25 +52,29 @@ const Dropcap = styled.div`
   color: ${colors.primary.gray.dark};
 `
 
+const Highlight = styled.span`
+  box-shadow: inset 0 -12px 0 0 ${themeGet('colors.highlight')};
+`
+
 const QuotesPage = ({data}) => {
   return (
     <Page title='Quotes' icon='quote'>
       {data.quotes.edges.map(({node}, i) => (
         <div key={i}>
-          <Container p={4} borderRadius={2}>
-            <Quote pt={2} pb={3}>
-              <Dropcap pr={4}>“</Dropcap>
-              {node.content}
+          <Container>
+            <Quote pb={4} mb={2}>
+              {/* <Dropcap pr={4}>“</Dropcap> */}
+              <Highlight>{node.content}</Highlight>
             </Quote>
 
-            <Meta p={3} m={-4}>
+            <Meta>
+              <Source>{node.metadata}</Source>
+
               <Tags mt={-2}>
                 {node.tags.map((tag, j) => (
                   <Token mt={2} mr={2} py={1} px={2} key={j}>{tag}</Token>
                 ))}
               </Tags>
-
-              <Source>{node.metadata}</Source>
             </Meta>
           </Container>
 
