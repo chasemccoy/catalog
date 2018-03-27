88181551cb4cf539cac54c5e940017807279f8c4
Chase McCoy <chasem000@gmail.com>
2018-03-26 20:33:30 -0500

    quoteyyyy

---
+++ src/components/Base.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import styled from 'styled-components'
+import { width } from 'styled-system'
+
+export const P = styled.p`
+	${width}
+`
--- src/pages/chicago.js
+++ src/pages/chicago.js
@@ -7,6 +7,7 @@ import styled from 'styled-components'
 import { Row, Column } from 'components/Grid'
 import MediaCard from 'components/MediaCard'
 import Mosaic from 'components/Mosaic'
+import { P } from 'components/Base'
 
 const ChicagoPage = ({data}) => {
   const categories = data.chicago.edges.map(a => a.node.category)
@@ -17,6 +18,8 @@ const ChicagoPage = ({data}) => {
 
   return (
     <Page wide title='Chicago' icon='chicago'>
+      <P width={[1, 1, 1, 3/4]}>I moved from a small college town in Mississippi to Chicago less than a year ago, in the middle of summer. And I've been in love since. There's so much more to see and so many more meals to eat. Here are some places that I am especially fond of, in no particular order.</P>
+
       <TableOfContents items={uniqueCategories} />
 
       {uniqueCategories.map((category, index) =>
