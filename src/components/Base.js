ddceb6f912bbdc1b77cb415a0cad3bbc4d59592d
Chase McCoy <chasem000@gmail.com>
2018-05-19 14:51:23 -0500

    new Books layout

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { width, color, space, fontSize } from 'styled-system'
+import { width, height, color, space, fontSize, boxShadow, borderRadius } from 'styled-system'
 
 export const P = styled.p`
   ${width} ${color} ${space} ${fontSize};
@@ -8,4 +8,12 @@ export const P = styled.p`
 
 export const Span = styled.span`
   ${color};
+`
+
+export const Box = styled.div`
+  ${space}
+  ${width}
+  ${height}
+  ${boxShadow}
+  ${borderRadius}
 `
