b5bb195aee7438c56b1658a321437cb1fe6aaddb
Chase McCoy <chasem000@gmail.com>
2019-02-23 16:47:07 -0600

    fix metadata

--- src/components/Metadata.js
+++ src/components/Metadata.js
@@ -5,7 +5,7 @@ import { useStaticQuery, graphql } from 'gatsby'
 const Metadata = ({ title, description, image, pathname, article, children }) => {
   const data = useStaticQuery(query) 
 
-  const { defaultDescription, twitterUsername, siteUrl } = data
+  const { defaultTitle, defaultDescription, titleTemplate, twitterUsername, siteUrl } = data.site.siteMetadata
 
   const seo = {
     title: title,
@@ -17,8 +17,8 @@ const Metadata = ({ title, description, image, pathname, article, children }) =>
     <React.Fragment>
       <Helmet 
         title={seo.title} 
-        titleTemplate='%s | Chase McCoy'
-        defaultTitle='Chase McCoy'
+        titleTemplate={titleTemplate} 
+        defaultTitle={defaultTitle}
       >
         <meta name="description" content={seo.description} />
 
