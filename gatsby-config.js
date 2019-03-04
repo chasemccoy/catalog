ff0c8596c92c66705535ac757605f45ef36b8f90
Chase McCoy <chasem000@gmail.com>
2019-03-03 20:04:43 -0600

    fix rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,13 +66,15 @@ module.exports = {
           {
             serialize: ({query: {site, allWordpressPost}}) => {
               return allWordpressPost.edges.map(edge => {
+                const siteUrl = site.siteMetadata.siteUrl.replace(/\/$/, '')
+
                 return Object.assign(
                   {},
                   {
                     title: edge.node.title === "" ? " " : edge.node.title,
                     description: edge.node.content,
-                    url: site.siteMetadata.siteUrl + edge.node.fields.fullSlug,
-                    guid: site.siteMetadata.siteUrl + edge.node.fields.fullSlug,
+                    url: siteUrl + edge.node.fields.fullSlug,
+                    guid: siteUrl + edge.node.fields.fullSlug,
                     date: edge.node.date,
                     author: 'Chase McCoy'
                   },
