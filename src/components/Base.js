aef9a5c089c57d531d17246e925f4041939a0642
Chase McCoy <chasem000@gmail.com>
2019-03-09 11:44:16 -0600

    try to get this building properly again

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Flex } from 'grid-styled'
 import { width, height, color, space, boxShadow, borderRadius, display, justifyContent, alignItems, flexWrap, flex, flexDirection, minWidth, minHeight, maxWidth } from 'styled-system'
 import theme from 'utils/theme'
 import media from 'utils/media'
@@ -26,7 +25,7 @@ export const Box = styled.div`
   ${maxWidth}
 `
 
-const GridFlex = styled(Flex)`
+const GridFlex = styled(Box)`
   & > * {
     padding-top: ${props => props.gutter ? `${props.theme.space[props.gutter] * 2}px` : `24px`};
     padding-left: ${props => props.gutter ? `${props.theme.space[props.gutter]}px` : `12px`};
@@ -49,6 +48,7 @@ const GridFlex = styled(Flex)`
 
 export const Grid = props => (
   <GridFlex
+    display='flex'
     mx={(props.gutter && [-props.gutter]) || ['-8px', '-12px']}
     mt={(props.gutter && [-theme.space[props.gutter] * 2]) || ['-16px', '-24px']}
     flexWrap={'wrap'}
