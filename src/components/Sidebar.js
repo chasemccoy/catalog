91a9ab90f2fa21abc2684ad5498afe9bdfeba1eb
Chase McCoy <chasem000@gmail.com>
2019-03-09 20:22:17 -0600

    refactor sidebar to use hooks

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Link from 'components/Link'
 import { Box, Text } from '@chasemccoy/kit'
@@ -53,181 +53,178 @@ const ToggleTheme = () => (
   </ThemeContext.Consumer>
 )
 
-class Sidebar extends React.Component {
-  state = {
-    open: false,
-    weatherSummary: null,
-    weatherTemperature: null,
-    nowPlayingName: null,
-    nowPlayingArtist: null,
-    beerName: null,
-    beerBrewery: null
-  }
+const getWeatherData = async (set) => {
+  const response = await fetch('https://chs-stats.now.sh/weather')
+  const result = await response.json()
+  set({
+    summary: result.summary,
+    temperature: result.temperature
+  })
+}
 
-  componentDidMount = () => {
-    // If we're loading up the site on a larger screen, expand the sidebar by default
-    window.matchMedia('(min-width: 768px)').matches &&
-      this.setState({ open: true })
+const getNowPlayingData = async (set) => {
+  const response = await fetch('https://chs-stats.now.sh/nowPlaying')
+  const result = await response.json()
+  set({
+    name: result.name,
+    artist: result.artist
+  })
+}
 
-    fetch(`https://chs-stats.now.sh/weather`)
-      .then(response => response.json())
-      .then(result => {
-        this.setState({ weatherSummary: result.summary })
-        this.setState({ weatherTemperature: result.temperature })
-      })
+const getTwitterData = async (set) => {
+  const response = await fetch('https://chs-stats.now.sh/latestTweet')
+  const result = await response.text()
+  set(result)
+}
 
-    fetch(`https://chs-stats.now.sh/nowPlaying`)
-      .then(response => response.json())
-      .then(result => {
-        this.setState({ nowPlayingName: result.name })
-        this.setState({ nowPlayingArtist: result.artist })
-      })
+const Sidebar = () => {
+  const [open, setOpen] = useState(false)
+  const [weather, setWeather] = useState(null)
+  const [nowPlaying, setNowPlaying] = useState(null)
+  const [tweet, setTweet] = useState(null)
 
-    // fetch(`https://chs-stats.now.sh/beer`)
-    //   .then(response => response.json())
-    //   .then(result => {
-    //     this.setState({ beerName: result.beer })
-    //     this.setState({ beerBrewery: result.brewery })
-    //   })
-  }
+  useEffect(() => {
+    window.matchMedia('(min-width: 768px)').matches && setOpen(true)
+  }, [])
 
-  toggleOpen = () => {
-    this.setState({ open: !this.state.open })
-  }
+  useEffect(() => {getWeatherData(setWeather)}, [])
+  useEffect(() => {getNowPlayingData(setNowPlaying)}, [])
+  useEffect(() => {getTwitterData(setTweet)}, [])
 
-  render() {
-    const { nowPlayingName, nowPlayingArtist, weatherTemperature } = this.state
+  return (
+    <Container>
+      <Header fontSize="16px" mb="8px" mt="-8px">
+        <Link to="/" unstyled>
+          CHASE McCOY
+        </Link>
 
-    return (
-      <Container {...this.props}>
-        <Header fontSize="16px" mb="8px" mt="-8px">
-          <Link to="/" unstyled>
-            CHASE McCOY
-          </Link>{' '}
-          <ToggleTheme />
-        </Header>
+        <ToggleTheme />
+      </Header>
 
-        <Text.p>
-          Thinking about design in the context of ethics, tooling, art, culture,
-          and computer science.
-        </Text.p>
+      <Text.p>
+        Designer and developer specializing in systems thinking, design tooling and front-end engineering.
+      </Text.p>
 
-        <Header>Get in touch</Header>
+      <Header>Get in touch</Header>
 
-        <Text.p width='95%'>
-          <Link to="https://twitter.com/chase_mccoy">Twitter</Link>,{' '}
-          <Link to="https://instagram.com/chs_mc">Instagram</Link>,{' '}
-          <Link to="https://github.com/chasemccoy">GitHub</Link>,{' '}
-          <Link to="mailto:desk@chasem.co">Email</Link>, &{' '}
-          <Link to="/feed.xml">RSS</Link>.
-        </Text.p>
+      <Text.p width='95%'>
+        <Link to="https://twitter.com/chase_mccoy">Twitter</Link>,{' '}
+        <Link to="https://instagram.com/chs_mc">Instagram</Link>,{' '}
+        <Link to="https://github.com/chasemccoy">GitHub</Link>,{' '}
+        <Link to="mailto:desk@chasem.co">Email</Link>, &{' '}
+        <Link to="/feed.xml">RSS</Link>.
+      </Text.p>
 
-        <Header>Browse</Header>
+      <Header>Browse</Header>
 
-        <UnorderedList unstyled mb={32}>
-          <li>
-            <Link to="/thoughts">/thoughts</Link>
-          </li>
-          <li>/resources</li>
-          <li>
-            <UnorderedList unstyled ml={4}>
-              <li>
-                <Link to="/books">/books</Link>
-              </li>
-              <li>
-                <Link to="/chicago">/chicago</Link>
-              </li>
-              <li>
-                <Link to="/quotes">/quotes</Link>
-              </li>
-            </UnorderedList>
-          </li>
-          <li>
-            <Link to="/portfolio">/portfolio</Link>
-          </li>
-        </UnorderedList>
-
-        {this.state.open ? (
-          <React.Fragment>
-            <Header>Currently</Header>
-            <UnorderedList mb={32}>
-              <li>
-                Working on <Link to='https://sproutsocial.com/seeds'>Seeds</Link> at <Link to='https://sproutsocial.com'>Sprout Social</Link>
-              </li>
-              <li>
-                {nowPlayingName && nowPlayingArtist ? (
-                  <React.Fragment>
-                    Listening to {nowPlayingName} by {nowPlayingArtist} on{' '}
-                    <Link to="https://open.spotify.com/user/22n2eydjrvftle33bi3t4v2pi?si=GAaVgz0FTk-4J4eUPNWBqQ">
-                      Spotify
-                    </Link>
-                  </React.Fragment>
-                ) : (
-                  'Loading...'
-                )}
-              </li>
-              <li>
-                {weatherTemperature ? (
-                  <React.Fragment>
-                    {weatherTemperature} in <Link to="/chicago">Chicago</Link>
-                  </React.Fragment>
-                ) : (
-                  'Loading...'
-                )}
-              </li>
-              {/* <li>{beerName && beerBrewery ? (
-                <React.Fragment>
-                  Drinking a {beerName} from {beerBrewery}
-                </React.Fragment>
-              ) : 'Loading...'}</li> */}
-            </UnorderedList>
-
-            <Header>Writing</Header>
-            <UnorderedList mb={3}>
+      <UnorderedList unstyled mb={32}>
+        <li>
+          <Link to="/thoughts">/thoughts</Link>
+        </li>
+        <li>/resources</li>
+        <li>
+          <UnorderedList unstyled ml={4}>
             <li>
-                <Link to="/2018/12/xoxo-2018">
-                  XOXO 2018
-                </Link>
-              </li>
-              <li>
-                <Link to="/2018/09/art-from-autonomy">
-                  Making Computers Make Art
-                </Link>
-              </li>
-              <li>
-                <Link to="/2018/06/some-prompted-thoughts-on-design">
-                  Some prompted thoughts on design
-                </Link>
-              </li>
-              <li>
-                <Link to="/2018/06/no-reservations">No Reservations</Link>
-              </li>
-              <li>
-                <Link to="/2018/05/exploring-seattle">Exploring Seattle</Link>
-              </li>
-            </UnorderedList>
+              <Link to="/books">/books</Link>
+            </li>
+            <li>
+              <Link to="/chicago">/chicago</Link>
+            </li>
+            <li>
+              <Link to="/quotes">/quotes</Link>
+            </li>
+          </UnorderedList>
+        </li>
+        <li>
+          <Link to="/portfolio">/portfolio</Link>
+        </li>
+      </UnorderedList>
 
-            <Link to="/thoughts" display="inline-block" mb={32}>
-              More →
-            </Link>
+      {open ? (
+        <React.Fragment>
+          <Header>Currently</Header>
+          <UnorderedList mb={32}>
+            <li>
+              Working on <Link to='https://sproutsocial.com/seeds'>Seeds</Link> at <Link to='https://sproutsocial.com'>Sprout Social</Link>
+            </li>
+            <li>
+              {nowPlaying ? (
+                <React.Fragment>
+                  Listening to {nowPlaying.name} by {nowPlaying.artist} on{' '}
+                  <Link to="https://open.spotify.com/user/22n2eydjrvftle33bi3t4v2pi?si=GAaVgz0FTk-4J4eUPNWBqQ">
+                    Spotify
+                  </Link>
+                </React.Fragment>
+              ) : (
+                'Loading...'
+              )}
+            </li>
+            <li>
+              {weather ? (
+                <React.Fragment>
+                  {weather.temperature} in <Link to="/chicago">Chicago</Link>
+                </React.Fragment>
+              ) : (
+                'Loading...'
+              )}
+            </li>
+          </UnorderedList>
 
-            <Header>Quoted</Header>
-            <Text.p mb="8px">
-              Be regular and orderly in your life, so that you may be violent
-              and original in your work.
-            </Text.p>
-            <Text.p mb={32}>
-              <em>— Gustave Flaubert</em>
-            </Text.p>
+          <Header>Writing</Header>
+          <UnorderedList mb={3}>
+          <li>
+              <Link to="/2018/12/xoxo-2018">
+                XOXO 2018
+              </Link>
+            </li>
+            <li>
+              <Link to="/2018/09/art-from-autonomy">
+                Making Computers Make Art
+              </Link>
+            </li>
+            <li>
+              <Link to="/2018/06/some-prompted-thoughts-on-design">
+                Some prompted thoughts on design
+              </Link>
+            </li>
+            <li>
+              <Link to="/2018/06/no-reservations">No Reservations</Link>
+            </li>
+            <li>
+              <Link to="/2018/05/exploring-seattle">Exploring Seattle</Link>
+            </li>
+          </UnorderedList>
 
-            <Button onClick={this.toggleOpen}>- collapse</Button>
-          </React.Fragment>
-        ) : (
-          <Button onClick={this.toggleOpen}>+ expand</Button>
-        )}
-      </Container>
-    )
-  }
+          <Link to="/thoughts" display="inline-block" mb={32}>
+            More →
+          </Link>
+
+          <Header>Quoted</Header>
+          <Text.p mb="8px">
+            Be regular and orderly in your life, so that you may be violent
+            and original in your work.
+          </Text.p>
+          <Text.p mb={32}>
+            <em>— Gustave Flaubert</em>
+          </Text.p>
+
+          <Header>Latest Tweet</Header>
+
+          <Text.p mb={2}>
+            {tweet || 'Loading...'}
+          </Text.p>
+
+          <Link to="https://twitter.com/chase_mccoy" display="block" mb={32}>
+            More →
+          </Link>
+
+          <Button onClick={() => setOpen(!open)}>- collapse</Button>
+        </React.Fragment>
+      ) : (
+        <Button onClick={() => setOpen(!open)}>+ expand</Button>
+      )}
+    </Container>
+  )
 }
 
 export default Sidebar
