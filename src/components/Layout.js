ee9fe89ec12edf4988e16ee9faaa88fcef4a9694
Chase McCoy <chasem000@gmail.com>
2019-09-14 16:47:08 -0500

    experimental dark mode

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,12 +4,27 @@ import theme from 'utils/theme'
 import { CSSReset } from '@chasemccoy/kit'
 import TypographyStyles from 'utils/typography'
 import Metadata from 'components/Metadata'
-// import SyntaxTheme from 'components/SyntaxTheme'
 import media from 'utils/media'
 import MDX from 'components/MDX'
 import 'isomorphic-fetch'
 
 const GlobalStyles = createGlobalStyle`
+  html {
+    background: white;
+
+    @media (prefers-color-scheme: dark) {
+      filter: invert(90%);
+
+      img, .no-invert {
+        filter: invert(100%);
+      }
+
+      pre {
+        filter: invert(4%);
+      }
+    }
+  }
+
   body {
     line-height: inherit;
     background: ${props => props.theme.colors.page.background};
