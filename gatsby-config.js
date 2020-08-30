77b378a921350b261903bf277696fa475e5dd860
Chase McCoy <chasem000@gmail.com>
2020-08-29 19:52:00 -0500

    update deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,44 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        extensions: ['.mdx', '.md'],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1400,
+              linkImagesToOriginal: true,
+              showCaptions: ['title']
+            }
+          },
+          {
+            resolve: 'gatsby-remark-autolink-headers',
+            options: {
+              icon: false
+            }
+          },
+          {
+            resolve: 'gatsby-remark-copy-linked-files'
+          },
+          {
+            resolve: 'gatsby-remark-smartypants'
+          }
+        ]
+      },
+      plugins: [
+        {
+          resolve: 'gatsby-remark-images',
+          options: {
+            maxWidth: 1400,
+            linkImagesToOriginal: true,
+            showCaptions: ['title']
+          }
+        }
+      ]
+    },
+    {
       resolve: require.resolve(`./themes/blog`)
     },
     {
--- themes/notes/gatsby-config.js
+++ themes/notes/gatsby-config.js
@@ -1,44 +1,6 @@
 module.exports = {
   plugins: [
     {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        extensions: ['.mdx', '.md'],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 1400,
-              linkImagesToOriginal: true,
-              showCaptions: ['title']
-            }
-          },
-          {
-            resolve: 'gatsby-remark-autolink-headers',
-            options: {
-              icon: false
-            }
-          },
-          {
-            resolve: 'gatsby-remark-copy-linked-files'
-          },
-          {
-            resolve: 'gatsby-remark-smartypants'
-          }
-        ],
-        plugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 1000,
-              linkImagesToOriginal: true,
-              showCaptions: ['title']
-            }
-          }
-        ]
-      }
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: 'notes',
