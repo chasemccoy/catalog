f6fd907a9197372ec26747d7a9078738f2da0da7
Chase McCoy <chasem000@gmail.com>
2020-09-19 14:02:04 -0500

    ~

---
+++ src/pages/thoughts/_index-old.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import Page from 'components/Page'
+import { Post } from 'components/Blog'
+import { Box } from '@chasemccoy/kit'
+import { graphql } from 'gatsby'
+import Wide from 'components/Wide'
+import Sidebar from 'components/Sidebar'
+
+const ThoughtsPage = ({ data }) => {
+  return (
+    <Page
+      title='Thoughts'
+      description="What's on my mind, and links to some interesting stuff on the web."
+      untitled
+      aside={<Sidebar />}
+    >
+      <Box>
+        {data.posts.nodes.map((node) => (
+          <React.Fragment key={node.id}>
+            <Box as='article' mb={[88, null, null, 120]}>
+              <Wide>
+                <Box
+                  height='6px'
+                  width={1}
+                  bg='accent.pop'
+                  mb={[-40, null, null, -32]}
+                />
+              </Wide>
+
+              <Post
+                title={node.title}
+                to={node.slug}
+                content={node.content}
+                excerpt={node.excerpt}
+                date={node.date}
+                isMdx={node.isMdx}
+                tags={node.tags}
+                aside={node.format === 'aside'}
+              />
+            </Box>
+          </React.Fragment>
+        ))}
+      </Box>
+    </Page>
+  )
+}
+
+export default ThoughtsPage
+
+export const query = graphql`
+  query ThoughtsLabsQuery {
+    posts: allBlog(
+      filter: { format: { nin: ["image", "aside"] } }
+      sort: { fields: date, order: DESC }
+      limit: 20
+    ) {
+      nodes {
+        id
+        title
+        date(formatString: "MMMM Do")
+        slug
+        content
+        excerpt
+        tags {
+          name
+        }
+        isMdx
+        format
+      }
+    }
+  }
+`
